4245c7978c0af03419f11bd1c6e6ffe25058b59a
HoverGo <kotikarbuz@gmail.com>
2023-10-28 17:01:52 +0500

    123

--- hovweb/main/views.py
+++ hovweb/main/views.py
@@ -4,5 +4,6 @@ from django.shortcuts import render
 def index(request):
     data = {
         "title": "Главная страница",
+        "hev": ["aaa", "ddd", "ccc"],
     }
     return render(request, "main/index.html", data)
